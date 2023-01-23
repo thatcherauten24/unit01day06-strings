7900edc32642fd56764da79b87df9ddc8f286bb4
Thatcher Auten <thatcherauten24@gmail.com>
2023-01-23 11:51:47 -0600

    initial commit

--- NegatePhrase.java
+++ NegatePhrase.java
@@ -1,5 +1,12 @@
 public class NegatePhrase {
     public static void main(String[] args) {
         
+        String phrase = args[0];
+        
+        if(phrase.substring(0, 2).equals("not")) {
+            System.out.println(phrase);
+        } else {
+            System.out.println("not " + phrase);
+        }
     }
 }
