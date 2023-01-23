64e086d1d3e08b5dd6641328e28499168049882c
Thatcher Auten <thatcherauten24@gmail.com>
2023-01-23 11:47:50 -0600

    testing

--- ShortLongShort.java
+++ ShortLongShort.java
@@ -1,5 +1,18 @@
 public class ShortLongShort {
     public static void main(String[] args) {
-        
+
+        String small = args[0];
+        String big = args[1];
+
+
+        if(small.length() < big.length()){
+            System.out.println(small + big + small);
+
+        } else {
+            System.out.println(big + small + big);
+        }
+
+
+
     }
 }
