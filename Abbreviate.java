98e5b9c6a5348c34eaea264ea86f751a82dd9bbe
Thatcher Auten <thatcherauten24@gmail.com>
2023-01-23 12:01:44 -0600

    initial commit

--- Abbreviate.java
+++ Abbreviate.java
@@ -1,5 +1,15 @@
 public class Abbreviate {
     public static void main(String[] args) {
         
+        String word = args[0];
+        String first2 = word.substring(0, 2);
+        String last2 = word.substring(word.length() - 2, word.length());
+        
+
+        if(word.length() < 8) {
+            System.out.println(word);
+        } else {
+            System.out.println(first2 + "..." + last2);
+        }
     }
 }
