f6a923a135dcbc2e1f3927cd7c15fa1451c453d1
Daniel Scislowski <mr.scislowski@gmail.com>
2023-01-31 19:34:38 -0600

    NegatePhrase.java feedback

--- NegatePhrase.java
+++ NegatePhrase.java
@@ -1,3 +1,5 @@
+// good, except:
+// TODO: this assignment asked you to actually create a new String (i.e. define it in a variable), and not just print a result
 public class NegatePhrase {
     public static void main(String[] args) {
         
